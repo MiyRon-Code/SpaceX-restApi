642a2f091e63f95abefb703599e573a4952e8ee6
Владислав Закалюжный <@new.local.itstep>
2021-02-18 14:46:42 +0600

    add pages

---
+++ src/pages/rockets.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="rockets">
+        <div class="container">
+            <div class="rockets-inner">
+                <h3>rockets</h3>
+                <Menu :mainRoute="'rockets'" :routes="['rockets-all']" :names="['все ракеты']" />
+                <router-view> </router-view>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Menu from '@/components/menu.vue'
+export default {
+     components:{
+           Menu,
+        },
+}
+
+</script>
---
+++ src/pages/rocketsAbout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="rocketsAbout">
+        <About 
+        :impPath="'https://cms.qz.com/wp-content/uploads/2017/02/spacex-falcon-9-landing-cape-canaveral-crs-10.jpg?quality=75&strip=all&w=1200&h=900&crop=1'"
+        :header ="'Rockets'"
+        :description="'Rockets - ракеты разработанные SpaceX '" />
+    </div>
+</template>
+
+<script>
+import About from '@/components/about.vue'
+
+export default {
+    components:{
+        About
+    }
+}
+</script>
