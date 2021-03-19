c290e39355ec35a69b06854cbe23daf8cd6dcf5a
MiyRon-Code <miyron.my@yandex.kz>
2021-03-19 15:46:15 +0600

    vuex modules update

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,23 @@
 //licence Apache 2.0
-
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios'
 Vue.use(Vuex);
 
 import {russian,english} from   '../store/lang.js'
 
+//modules
 import apiModule from '../store/modules/apiModule.js'
-
 import * as THREE from 'three';
 
 export default   new Vuex.Store({
-
   state: {
     scene:  1,
     
     //loader
     loader: false,
-
-    dragons: null,
-    landpads: null,
-    launches: null,
-    launchesUpcoming: null,
-    launchesPast: null,
-    rockets:null,
-    missions: false,
+    
     //локализация 
-    //по умолчание engish
-    local: english
-
+    local: english //по умолчание engish
   },
   actions: {
     
@@ -42,83 +30,6 @@ export default   new Vuex.Store({
     async plsClearScene(ctx){
       console.log("scene will clear")
       await ctx.commit('clearScene')
-    },
-    
-
-    //dragons
-    fetchDragonsAll(ctx){
-      axios.get('https://api.spacexdata.com/v3/dragons')
-      .then(function (response) {
-          ctx.commit('updateDragons',response)
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
-    },
-
-
-      
-
-    //landpads
-    fetchLandpadsAll(ctx){
-      axios.get('https://api.spacexdata.com/v3/launchpads')
-      .then(function (response) {
-          ctx.commit('updateLandpads',response)
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
-    },
-
-    //launches
-    fetchLaunchesAll(ctx){
-      axios.get('https://api.spacexdata.com/v3/launches')
-      .then(function (response) {
-          ctx.commit('updateLaunches',response)
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
-    },
-    fetchLaunchesPast(ctx){
-      axios.get('https://api.spacexdata.com/v3/launches/past')
-      .then(function (response) {
-          ctx.commit('updateLaunchesPast',response)
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
-    },
-    fetchLaunchesUpcoming(ctx){
-      axios.get('https://api.spacexdata.com/v3/launches/upcoming')
-      .then(function (response) {
-          ctx.commit('updateLaunchesUpcoming',response)
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
-    },
-
-    //rockets
-    fetchRockets(ctx){
-      axios.get('https://api.spacex.land/rest/rockets')
-      .then(function (response) {
-          ctx.commit('updateRockets',response)
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
-    },
-
-    //missions
-    fetchMissions(ctx){
-      axios.get('https://api.spacexdata.com/v3/missions')
-      .then(function (response) {
-          ctx.commit('updateMissions',response)
-      })
-      .catch(function (error) {
-          console.log(error);
-      })
     },
 
     changeLocalLang(ctx){
@@ -142,96 +53,22 @@ export default   new Vuex.Store({
       }
     },
 
-
     updateLocalLang(state, lang){
       state.local = lang;
     },
-    //missions
-    updateMissions(state, missions){
-      state.missions = missions;
-    },
-
-
-
-
-    
-
-
-
-    //dragons Update
-    updateDragons(state, dragons){
-      state.dragons = dragons;
-    },
-
-    //landpads
-    updateLandpads(state, landpads){
-      state.landpads = landpads;
-    },
-
-    //launches
-    updateLaunches(state, launches){
-      state.launches = launches;
-    },
-    
-    updateLaunchesUpcoming(state, launches){
-      state.launchesUpcoming = launches;
-    },
-    updateLaunchesPast(state, launches){
-      state.launchesPast = launches;
-    },
-    //rockets
-    updateRockets(state, rockets){
-      state.rockets = rockets;
-    },
   },
   getters: {
+    //THREE SCENE
     getScene: state =>{
       return state.scene;
     },
     
-
     //loaderStatus get
     getLoaderStatus: state => {
       return  state.loader
     },
 
-    
-
-    //dragons get
-    getDragons: state => {
-      return  state.dragons
-    },
-
-    //landpads get
-    getLandpads: state => {
-      return  state.landpads
-    },
-
-
-    //launches get
-    getLaunches: state => {
-      return  state.launches
-    },
-
-    getLaunchesUpcoming: state => {
-      return  state.launchesUpcoming
-    },
-
-    getLaunchesPast: state => {
-      return  state.launchesPast
-    },
-
-    //Rockets get 
-    getRockets: state => {
-      return  state.rockets
-    },
-
-    //Missions get 
-    getMissions: state => {
-      return  state.missions
-    },
-
-    //getLocal
+    //LOCALIZATION
     getLocalCategory: state =>{
       return state.local.category
     },
--- src/store/modules/apiModule.js
+++ src/store/modules/apiModule.js
@@ -6,8 +6,13 @@ export default{
       capsules: null,
       capsulesPast: null,
       capsulesUpcoming: null,
-      
-      
+      dragons: null,      
+      landpads: null,
+      launches: null,
+      launchesUpcoming: null,
+      launchesPast: null,   
+      rockets:null,
+      missions: false, 
     }),
     actions: {
       //aboutCompany
@@ -51,6 +56,80 @@ export default{
           console.log(error);
       })
     },
+
+    //dragons
+    fetchDragonsAll({commit}){
+      axios.get('https://api.spacexdata.com/v3/dragons')
+      .then(function (response) {
+          commit('updateDragons',response)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+    },
+    
+    //landpads
+    fetchLandpadsAll({commit}){
+      axios.get('https://api.spacexdata.com/v3/launchpads')
+      .then(function (response) {
+          commit('updateLandpads',response)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+    },
+
+    //launches
+    fetchLaunchesAll({commit}){
+      axios.get('https://api.spacexdata.com/v3/launches')
+      .then(function (response) {
+          commit('updateLaunches',response)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+    },
+    fetchLaunchesPast({commit}){
+      axios.get('https://api.spacexdata.com/v3/launches/past')
+      .then(function (response) {
+          commit('updateLaunchesPast',response)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+    },
+    fetchLaunchesUpcoming({commit}){
+      axios.get('https://api.spacexdata.com/v3/launches/upcoming')
+      .then(function (response) {
+          commit('updateLaunchesUpcoming',response)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+    },
+
+    //rockets
+    fetchRockets({commit}){
+      axios.get('https://api.spacex.land/rest/rockets')
+      .then(function (response) {
+          commit('updateRockets',response)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+    },
+
+    //missions
+    fetchMissions({commit}){
+      axios.get('https://api.spacexdata.com/v3/missions')
+      .then(function (response) {
+          commit('updateMissions',response)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+    },
+
   },
     mutations: {
       //company
@@ -67,6 +146,36 @@ export default{
       },
       updateCapsulesUpcoming(state, capsules){
         state.capsulesUpcoming = capsules;
+      },
+      
+      //dragons Update
+      updateDragons(state, dragons){
+        state.dragons = dragons;
+      },
+
+      //landpads
+      updateLandpads(state, landpads){
+        state.landpads = landpads;
+      },
+
+      //launches
+      updateLaunches(state, launches){
+        state.launches = launches;
+      },
+      updateLaunchesUpcoming(state, launches){
+        state.launchesUpcoming = launches;
+      },
+      updateLaunchesPast(state, launches){
+        state.launchesPast = launches;
+      },
+
+      //rockets
+      updateRockets(state, rockets){
+        state.rockets = rockets;
+      },
+      //missions
+      updateMissions(state, missions){
+        state.missions = missions;
       },
     },
 
@@ -86,5 +195,38 @@ export default{
       getUpcomingCapsules: state => {
         return  state.capsulesUpcoming
       },
-     }
+
+      //dragons get
+      getDragons: state => {
+        return  state.dragons
+      },
+
+      //landpads get
+      getLandpads: state => {
+        return  state.landpads
+      },
+
+      //launches get
+      getLaunches: state => {
+        return  state.launches
+      },
+
+      getLaunchesUpcoming: state => {
+        return  state.launchesUpcoming
+      },
+
+      getLaunchesPast: state => {
+        return  state.launchesPast
+      },
+
+      //Rockets get 
+      getRockets: state => {
+        return  state.rockets
+      },
+
+      //Missions get 
+      getMissions: state => {
+        return  state.missions
+      },
+    }
   }
